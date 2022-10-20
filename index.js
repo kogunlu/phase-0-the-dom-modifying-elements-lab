ba742acedf1ed82dcc1df151a9ef946590b41e77
Kuday Ogunlu <kudayogunlu@gmail.com>
2022-10-20 10:58:08 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const mainElement = document.querySelector('#main')
+mainElement.remove()
+
+const newHeader = document.createElement("h1")
+
+newHeader.id = "victory"
+
+newHeader.textContent = "Kuday is the champion"
